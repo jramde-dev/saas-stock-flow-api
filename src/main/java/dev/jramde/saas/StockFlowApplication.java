ae454e0323ccf45bb8f4db129252647968946418
Juldas RAMDE <juldas1.ramde@orange.com>
2026-04-13 15:11:38 +0000

    init!: generate project

---
+++ src/main/java/dev/jramde/saas/StockFlowApplication.java
@@ -0,0 +1,13 @@
+package dev.jramde.saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockFlowApplication.class, args);
+	}
+
+}
