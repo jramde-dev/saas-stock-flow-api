33b108ece09fa2ca9d9cb7440fbdc7d6f3e75995
Juldas RAMDE <juldas1.ramde@orange.com>
2026-04-20 09:36:39 +0000

    feat!: implement security filter chain

---
+++ src/main/java/dev/jramde/saas/security/JrSecurityConfig.java
@@ -0,0 +1,79 @@
+package dev.jramde.saas.security;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity // Pour sécuriser les #ts endpoints
+@RequiredArgsConstructor
+public class JrSecurityConfig {
+    private final JrJwtAuthenticationFilter jwtAuthenticationFilter;
+    private static final String[] PUBLIC_URLS = {
+            "/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigSource()))
+                .authorizeHttpRequests(auth -> auth
+                        // Autoriser les requêtes publiques
+                        .requestMatchers(PUBLIC_URLS)
+                        .permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**")
+                        .permitAll()
+
+                        // Demander l'authentification pour toutes les autres requêtes
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Exécuter le filtre JWT avant le filtre d'authentification
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    /**
+     * Spécifier à Spring les méthodes qu'on veut autoriser.
+     *
+     * @return CorsConfigurationSource
+     */
+    @Bean
+    private CorsConfigurationSource corsConfigSource() {
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*")); // Do not do this in production
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(3600L);
+
+        // Définir sur quelle source on veut appliquer cette config
+        final UrlBasedCorsConfigurationSource urlBasedConfigSource = new UrlBasedCorsConfigurationSource();
+        urlBasedConfigSource.registerCorsConfiguration("/**", corsConfiguration);
+        return urlBasedConfigSource;
+    }
+}
